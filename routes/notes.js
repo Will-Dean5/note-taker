f997f2bb18285bbca43c84d9cb81565fad82f537
Will D <will.dean0103@gmail.com>
2022-09-16 19:02:01 -0400

    worked on notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,28 @@
-const app = require('express').Router();
+const apps = require('express').Router();
 const {readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
 // const fs = require('fs');
 const uuid = require('../helpers/uuid');
 
 
-app.get('/', (req, res) => {
+apps.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => 
     res.json(JSON.parse(data)))
 });
 
-app.post('/', (req, res) => {
+apps.delete('/:id', async (req, res) => {
+    const deleteNote = req.params.id;
+    let readFile = await readFromFile('./db/db.json');
+    readFile = JSON.parse(readFile);
+    readFile = readFile.filter(others => others.id !== deleteNote);
+    writeToFile('./db/db.json', readFile);
+    res.json('You deleted your note');
+})
+
+apps.post('/', (req, res) => {
     console.log(req.body);
     const {title, text} = req.body;
     if(req.body) {
-        const note = {title, text, id: uuid()};
+        const note = {title, text, id: uuid(),};
         readAndAppend(note, './db/db.json');
         res.json('Note added');
     } else {
@@ -21,4 +30,4 @@ app.post('/', (req, res) => {
     }
 });
 
-module.exports = app;
+module.exports = apps;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use('/api', api);
 
 app.get('/', (req, res) => 
-    res.sendFile(path,join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) => 
